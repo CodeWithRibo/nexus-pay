49a6a76dc7480b806148fc69f68a35aac24974e8
Carl John Sto. Tomas <riboluna2818@gmail.com>
2026-03-13 20:30:18 +0800

    feat: add dynamic title support with app name in Inertia app initialization

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
 
 createInertiaApp({
+    title: (title) => `${title} | Nexus Pay`,
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
         return pages[`./Pages/${name}.vue`];
