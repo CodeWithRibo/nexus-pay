202343c633867eedcc7d88fb7fe6425e01d408b5
Carl John Sto. Tomas <riboluna2818@gmail.com>
2026-03-18 22:18:50 +0800

    feat: add TuitionFee page with KioskLayout, HeaderSection, BillSummary, and Footer components

---
+++ resources/js/Pages/kiosk/TuitionFee.vue
@@ -0,0 +1,20 @@
+<script setup>
+import KioskLayout from "@/Pages/components/layout/KioskLayout.vue";
+import { Head } from "@inertiajs/vue3";
+import BillSummary from "@/Pages/components/kiosk/BillSummary.vue";
+import HeaderSection from "@/Pages/components/kiosk/HeaderSection.vue";
+import Footer from "@/Pages/components/layout/kiosk/Footer.vue";
+</script>
+
+<template>
+    <KioskLayout>
+        <Head title="Payment Method" />
+        <div class="min-h-screen flex flex-col">
+            <HeaderSection />
+            <main class="flex-1 flex overflow-hidden">
+                <BillSummary />
+            </main>
+            <Footer />
+        </div>
+    </KioskLayout>
+</template>
