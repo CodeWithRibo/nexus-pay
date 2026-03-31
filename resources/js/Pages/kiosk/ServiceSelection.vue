9f7365082abf51863877f87547988cf54eff0d56
CodeWithRibo <riboluna2818@gmail.com>
2026-04-01 00:17:29 +0800

    feat: restructure ServiceSelection layout with header and footer

--- resources/js/Pages/kiosk/ServiceSelection.vue
+++ resources/js/Pages/kiosk/ServiceSelection.vue
@@ -3,6 +3,8 @@ import KioskLayout from "@/Pages/components/layout/KioskLayout.vue";
 import { Head, usePage } from "@inertiajs/vue3";
 import ServiceCard from "@/Pages/components/kiosk/ServiceCard.vue";
 import { computed } from "vue";
+import HeaderSection from "@/Pages/components/layout/kiosk/HeaderSection.vue";
+import Footer from "@/Pages/components/layout/kiosk/Footer.vue";
 
 const page = usePage();
 const user = computed(() => page.props.auth.user);
@@ -11,11 +13,16 @@ const isAuth = computed(() => Boolean(!user.value));
 </script>
 
 <template>
-    <KioskLayout
-        class="w-full flex items-center justify-center relative min-h-screen"
-    >
+    <KioskLayout>
         <Head title="Service" />
-        <ServiceCard />
+        <div class="min-h-screen flex flex-col">
+            <HeaderSection/>
+            <main class="mt-10">
+                <ServiceCard />
+            </main>
+            <Footer/>
+        </div>
+
     </KioskLayout>
 </template>
 
