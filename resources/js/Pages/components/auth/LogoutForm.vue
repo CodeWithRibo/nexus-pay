12a90a8a7ed18f1b001e442a20d6569a1ab9a31f
Carl John Sto. Tomas <riboluna2818@gmail.com>
2026-03-31 11:02:03 +0800

    feat: add LogoutForm component with form submission for logging out

---
+++ resources/js/Pages/components/auth/LogoutForm.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { Button } from "@/components/ui/button/index.js";
+import { useForm } from "@inertiajs/vue3";
+
+const form = useForm();
+
+const submitForm = () => {
+    form.post(route("login.destroy"));
+};
+</script>
+
+<template>
+    <form @submit.prevent="submitForm">
+        <Button type="submit">Logout</Button>
+    </form>
+</template>
