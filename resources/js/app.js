7870815a0f4c7675268e0c16b72b2a92f11279da
CodeWithRibo <riboluna2818@gmail.com>
2026-04-01 00:09:15 +0800

    fix: update page resolution to use dynamic import for Vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,8 +9,8 @@ createInertiaApp({
     },
     title: (title) => `${title} - Nexus Pay`,
     resolve: (name) => {
-        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
-        return pages[`./Pages/${name}.vue`];
+        const pages = import.meta.glob("./Pages/**/*.vue");
+        return pages[`./Pages/${name}.vue`]();
     },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
