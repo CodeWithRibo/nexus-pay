85218a44a910e0e74e591b519dbbd151969130ea
CodeWithRibo <riboluna2818@gmail.com>
2026-04-01 00:14:18 +0800

    feat: calculate and pass student amount due to CashInsertion component

--- app/Http/Controllers/Kiosk/CashInsertionController.php
+++ app/Http/Controllers/Kiosk/CashInsertionController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Kiosk;
 
 use App\Http\Controllers\Controller;
+use App\Models\StudentBalance;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 
 class CashInsertionController extends Controller
 {
     public function __invoke()
     {
-        return Inertia::render('kiosk/CashInsertion');
+        $studAmountDue = StudentBalance::query()
+        ->where('user_id', auth()->id())
+        ->where('fee_name', 'Tuition Fee')
+        ->sum('total_amount');
+
+        return Inertia::render('kiosk/CashInsertion', compact('studAmountDue'));
     }
 }
