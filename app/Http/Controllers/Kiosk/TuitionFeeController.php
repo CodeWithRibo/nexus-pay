408ed0b80d17fd4b993bc60842f18f7a1b3bddfe
CodeWithRibo <riboluna2818@gmail.com>
2026-04-01 21:04:39 +0800

    feat: add transaction ID generation and payment creation in TuitionFeeController

--- app/Http/Controllers/Kiosk/TuitionFeeController.php
+++ app/Http/Controllers/Kiosk/TuitionFeeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Kiosk;
 
 use App\Http\Controllers\Controller;
+use App\Models\Payment;
 use App\Models\User;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 use function Pest\Laravel\get;
 
@@ -11,6 +13,15 @@ class TuitionFeeController extends Controller
 {
     public function __invoke()
     {
+
+        $transactionId = Str::uuid7()->toString();
+
+        Payment::query()->create([
+            'transaction_id' => $transactionId,
+            'user_id' => auth()->id(),
+            'status' => 'pending',
+        ]);
+
         $student = User::query()
             ->where('id', auth()->id())
             ->with([
@@ -28,7 +39,8 @@ class TuitionFeeController extends Controller
                 'student_name' => $student->information->first_name.' '.$student->information->last_name ?? 'Unknown',
                 'student_id' => $student->student_id,
                 'description' => $student->studentBalances->first()->fee_name,
-                'current_balance' => optional($student->studentBalances->first())->total_amount
+                'current_balance' => optional($student->studentBalances->first())->total_amount,
+                'transaction_id' => $transactionId,
             ]
         ]);
     }
