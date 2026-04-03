e2a5e0597da963192e334442b9380bdc44a9fa11
Carl John Sto. Tomas <riboluna2818@gmail.com>
2026-04-04 02:24:52 +0800

    chore: remove unused Payment model logic and clean up TuitionFeeController

--- app/Http/Controllers/Kiosk/TuitionFeeController.php
+++ app/Http/Controllers/Kiosk/TuitionFeeController.php
@@ -3,25 +3,13 @@
 namespace App\Http\Controllers\Kiosk;
 
 use App\Http\Controllers\Controller;
-use App\Models\Payment;
 use App\Models\User;
-use Illuminate\Support\Str;
 use Inertia\Inertia;
-use function Pest\Laravel\get;
 
 class TuitionFeeController extends Controller
 {
     public function __invoke()
     {
-
-        $transactionId = Str::uuid7()->toString();
-
-        Payment::query()->create([
-            'transaction_id' => $transactionId,
-            'user_id' => auth()->id(),
-            'status' => 'pending',
-        ]);
-
         $student = User::query()
             ->where('id', auth()->id())
             ->with([
@@ -40,7 +28,6 @@ class TuitionFeeController extends Controller
                 'student_id' => $student->student_id,
                 'description' => $student->studentBalances->first()->fee_name,
                 'current_balance' => optional($student->studentBalances->first())->total_amount,
-                'transaction_id' => $transactionId,
             ]
         ]);
     }
