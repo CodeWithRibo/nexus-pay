2881e3b56ca983b3ecf7da64439d2649e00b06c1
Carl John Sto. Tomas <riboluna2818@gmail.com>
2026-04-03 20:57:59 +0800

    fix: load payment details in ReceiptController and accept transaction ID

--- app/Http/Controllers/Kiosk/ReceiptController.php
+++ app/Http/Controllers/Kiosk/ReceiptController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Kiosk;
 
 use App\Http\Controllers\Controller;
+use App\Models\Payment;
 use Inertia\Inertia;
 
 class ReceiptController extends Controller
 {
-    public function __invoke()
+    public function __invoke($transaction_id)
     {
+        $payment = Payment::query()
+            ->where('transaction_id', $transaction_id)
+            ->where('user_id', auth()->id())
+            ->firstOrFail();
+
+//        if ($payment->status !== 'pending') {
+//            return redirect()->route('kiosk.landing-screen');
+//        }
+
         return Inertia::render('kiosk/Receipt');
     }
 }
