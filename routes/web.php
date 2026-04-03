d2d2b8c1c791336950b388ac05a500e61ff6e19d
Carl John Sto. Tomas <riboluna2818@gmail.com>
2026-04-04 02:24:42 +0800

    feat: add routes for initiating and starting payment processing in TuitionFee module

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Kiosk\ReceiptController;
 use App\Http\Controllers\Kiosk\CheckBalanceController;
 use App\Http\Controllers\Kiosk\ServiceSelectionController;
 use App\Http\Controllers\Kiosk\TuitionFeeController;
+use App\Http\Controllers\Kiosk\InitiatePaymentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -45,7 +46,9 @@ Route::get('/{isLoggedIn?}', LandingScreenController::class)
 
 Route::middleware(['auth', 'student'])->group(function() {
     Route::get('kiosk/tuition-fee/payment-method', TuitionFeeController::class)->name('kiosk.tuition-fee.payment-method');
+    Route::post('kiosk/tuition-fee/initiate-payment', InitiatePaymentController::class)->name('kiosk.tuition-fee.initiate-payment');
     Route::get('kiosk/tuition-fee/cash-insertion/{transaction_id}', CashInsertionController::class)->name('kiosk.tuition-fee.cash-insertion');
+    Route::post('kiosk/tuition-fee/start-processing/{transaction_id}', [ProcessingPaymentController::class, 'start'])->name('kiosk.tuition-fee.processing.start');
     Route::post('kiosk/tuition-fee/processing/{transaction_id}', [ProcessingPaymentController::class, 'process'])->name('kiosk.tuition-fee.processing.process');
     Route::get('kiosk/tuition-fee/processing/{transaction_id}', [ProcessingPaymentController::class, 'index'])->name('kiosk.tuition-fee.processing.index');
     Route::get('kiosk/tuition-fee/receipt/{transaction_id}', ReceiptController::class)->name('kiosk.tuition-fee.receipt');
