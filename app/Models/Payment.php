d89ea4c082f76aca033062e63e6c2804895b4b87
Carl John Sto. Tomas <riboluna2818@gmail.com>
2026-04-05 03:41:58 +0800

    refactor: remove SoftDeletes trait from Payment model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Payment extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'reference_no',
