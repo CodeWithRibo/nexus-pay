03c33ca1ef4a47a6a18df7463a78c885f01e9417
Carl John Sto. Tomas <riboluna2818@gmail.com>
2026-04-05 03:41:35 +0800

    feat: integrate IdleScanner in CheckBalance page for auth-based auto-logout functionality

--- resources/js/Pages/kiosk/CheckBalance.vue
+++ resources/js/Pages/kiosk/CheckBalance.vue
@@ -1,14 +1,20 @@
 <script setup>
 import KioskLayout from "@/Pages/components/layout/KioskLayout.vue";
-import { Head } from "@inertiajs/vue3";
+import { Head, usePage } from "@inertiajs/vue3";
 import OutstandingBalances from "@/Pages/components/kiosk/OutstandingBalances.vue";
 import HeaderSection from "@/Pages/components/layout/kiosk/HeaderSection.vue";
 import Footer from "@/Pages/components/layout/kiosk/Footer.vue";
+import IdleScanner from "@/components/IdleScanner.vue";
+import { computed } from "vue";
+
+const page = usePage();
+const user = computed(() => page.props.auth.user === null);
+const isAuth = computed(() => Boolean(!user.value));
 </script>
 
 <template>
+    <IdleScanner v-if="isAuth" />
     <KioskLayout>
-        <!--Show list of outstanding balance-->
         <Head title="Balances" />
         <div class="min-h-screen flex flex-col">
             <HeaderSection />
