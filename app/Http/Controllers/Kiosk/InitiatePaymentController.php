1f422cae5a15731f65a7d4b64e9ac6bfbfcb14e0
CodeWithRibo <riboluna2818@gmail.com>
2026-04-05 17:16:10 +0800

    feat: add initiate payment method in InitiatePaymentController for kiosk transactions

--- app/Http/Controllers/Kiosk/InitiatePaymentController.php
+++ app/Http/Controllers/Kiosk/InitiatePaymentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Kiosk;
 
 use App\Http\Controllers\Controller;
 use App\Models\Payment;
+use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class InitiatePaymentController extends Controller
@@ -21,6 +22,30 @@ class InitiatePaymentController extends Controller
         session(['transaction_id' => $transactionId]);
 
         return redirect()->route('kiosk.tuition-fee.cash-insertion', [
+            'transaction_id' => $transactionId,
+        ]);
+    }
+
+    public function initiate(Request $request)
+    {
+        $transactionId = Str::uuid7()->toString();
+
+        $balanceId = $request->input('balance_id');
+        $payAll = $request->boolean('pay_all', false);
+
+        Payment::query()->create([
+            'transaction_id' => $transactionId,
+            'user_id' => auth()->id(),
+            'status' => 'pending',
+        ]);
+
+        session([
+            'transaction_id' => $transactionId,
+            'payment_balance_id' => $balanceId ? (int) $balanceId : null,
+            'payment_pay_all' => (bool) $payAll,
+        ]);
+
+        return redirect()->route('kiosk.cash-insertion', [
             'transaction_id' => $transactionId,
         ]);
     }
