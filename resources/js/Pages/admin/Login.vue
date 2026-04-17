fb65f6a241ae78563d4cf812606c202a258d46d3
Carl John Sto. Tomas <riboluna2818@gmail.com>
2026-04-17 13:38:12 +0800

    Add admin login page with KioskLayout and LoginForm integration

---
+++ resources/js/Pages/admin/Login.vue
@@ -0,0 +1,30 @@
+<script setup>
+import LoginForm from "@/Pages/components/auth/LoginForm.vue";
+import { Head, useForm } from "@inertiajs/vue3";
+import KioskLayout from "@/Pages/components/layout/KioskLayout.vue";
+
+const props = defineProps({
+    role: {
+        type: String,
+        default: "admin",
+    },
+});
+
+const form = useForm({
+    login: null,
+    password: null,
+});
+
+const submitForm = () => {
+    form.post(route("admin.login.store"), {
+        onError: () => {},
+    });
+};
+</script>
+
+<template>
+    <KioskLayout>
+        <Head title="Admin Login" />
+        <LoginForm :form="form" :user-type="props.role" @submit="submitForm" />
+    </KioskLayout>
+</template>
